8ac04820445fe8b29e85590eea1b434887d1105b
Liza Deka <56999749+Baba-Yaga-1@users.noreply.github.com>
2021-09-13 01:15:11 +0530

    Add files via upload

---
+++ Recipe Finder/restaurant.js
@@ -0,0 +1,19 @@
+document.getElementById('button4').addEventListener('click',getAPI2);
+function getAPI2(){
+    //fetch('https://api.github.com/users/jainapoorva40')
+    fetch('https://www.themealdb.com/api/json/v1/1/search.php?s')
+    //https://type.fit/api/quotes
+    .then(function(res){
+        return res.json();
+    })
+    .then(function(data){
+        console.log(data);
+        let outpu=``;
+        data.forEach(function(meals){
+            outpu+=`<li>${meals.strInstructions}</li>`;
+        })
+    
+    document.getElementById('output').innerHTML=outpu;
+    })
+    e.preventDefault(); 
+}
